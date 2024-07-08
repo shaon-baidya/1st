e8089b657facb2bcceafdbe1e269123f45609cc3
shaon-baidya <147084654+shaon-baidya@users.noreply.github.com>
2024-07-08 10:14:32 +0000

    abount my account

---
+++ basics/1st.js
@@ -0,0 +1,5 @@
+const accountId = 908864
+let accountEmail = "shaon.baidya2000@gmail.com"
+var accountPassword = "12345"
+accountCity = "kolkata"
+console.table([accountId, accountEmail, accountPassword, accountCity])
